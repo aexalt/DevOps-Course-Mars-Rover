c8661d55fbed2d55371cac1f64a53106a5f9d299
aexalt <xandyt@gmail.com>
2022-02-09 16:03:23 +0000

    first

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
+import requests
 from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
+nasa_api_key = "YsD8TsWqhkTlB5hzRkuOa6fl173kYpKHTgmbHpBL"
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    daterequested = request.args["date"]
+    response = requests.get("https://api.nasa.gov/planetary/apod?date=" + daterequested + "&api_key=" + nasa_api_key)
+    url = response.json()["url"]
+
+    return render_template('index.html', landing_image=url, explanation=response.json()["explanation"],media_type=response.json()["media_type"])
 
 @app.route('/mars')
 def mars():
-    return render_template('mars.html')
+    response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=" + nasa_api_key)
+    photoArray = response.json()["photos"]
+    print(photoArray)
+    return render_template('mars.html', photo_array=photoArray)
